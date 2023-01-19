5d6afe682b8d6a65c543b92d0b4ce70a8af32de1
chaechae <songchaechaes@gmail.com>
2023-01-19 17:53:33 +0900

    feat: add tag list component

---
+++ src/components/modules/article/TagList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="tag-list">
+    <li
+      class="tag-default tag-pill tag-outline"
+      v-for="(value, index) in tagList"
+      :key="index"
+    >
+      {{ value }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    tagList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style></style>
