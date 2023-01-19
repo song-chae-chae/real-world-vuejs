706072a0a91c9d2e4e20beae379777069cdea4ef
chaechae <songchaechaes@gmail.com>
2023-01-19 17:40:42 +0900

    feat: set login user in vuex

---
+++ src/store/constant.js
@@ -0,0 +1,4 @@
+export default {
+  SET_USER: "SET_USER",
+  GET_USER: "GET_USER",
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,33 @@
+import {getUser} from "@/api/user";
 import Vue from "vue";
 import Vuex from "vuex";
+import Constant from "./constant";
 
 Vue.use(Vuex);
 
+const initialState = {
+  user: {},
+};
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    ...initialState,
+  },
+  getters: {
+    getCurrentUser: function (state) {
+      return state.user;
+    },
+  },
+  mutations: {
+    [Constant.SET_USER]: (state, data) => {
+      state.user = data;
+    },
+  },
+  actions: {
+    [Constant.GET_USER]: function () {
+      return getUser().then((result) => {
+        this.commit(Constant.SET_USER, result.data);
+      });
+    },
+  },
   modules: {},
 });
